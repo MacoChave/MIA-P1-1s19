db04b3ce774ad99eedad6891251b14fe45fd4620
Maco Chave <catalantt@gmail.com>
2019-02-14 12:43:34 -0600

    Obtener nombre y extension de un path

--- file_manager/manager.h
+++ file_manager/manager.h
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include "../struct/info_disk.h"
 
+#define _DISK_ 0
+#define _PNG_ 1
+#define _JPG_ 2
+
 char * getDirectoryPath (char * filename)
 {
     char * s = filename;
@@ -35,12 +39,56 @@ char * getDirectoryPath (char * filename)
 
 char * getName (char * filename)
 {
+    char * s = filename;
 
+    char * name = (char *)malloc(strlen(filename));
+    memset(name, 0, strlen(filename));
+    while(*s)
+    {
+        if (*s != '/')
+            sprintf(name, "%s%c", name, *s);
+        else if (*s == '/')
+            memset(name, 0, strlen(filename));    
+        *s++;
+    }
+
+    return name;
 }
 
-char * getExtension (char * filename)
+int getExtensionNumber (char * extension)
 {
-    
+    if (strcmp(extension, "disk") == 0)
+        return _DISK_;
+    if (strcmp(extension, "dsk") == 0)
+        return _DISK_;
+    if (strcmp(extension, "png") == 0)
+        return _PNG_;
+    if (strcmp(extension, "jpeg") == 0)
+        return _JPG_;
+    return -1;
+}
+
+int getExtension (char * filename)
+{
+    char * s = filename;
+
+    char * extension = (char *)malloc(strlen(filename));
+    memset(extension, 0, strlen(filename));
+
+    while(*s)
+    {
+        if (*s != '/' && *s != '.')
+            sprintf(extension, "%s%c", extension, *s);
+        else if (*s == '/' || *s == '.')
+            memset(extension, 0, strlen(filename));
+        
+        *s++;
+    }
+    int extension_number = getExtensionNumber(extension);
+    free(extension);
+    extension = NULL;
+
+    return extension_number;
 }
 
 #endif // MANAGER_H_INCLUDED
