f769064cbf8d70a24e42e24613b3bebcd501aa3b
Maco Chave <catalantt@gmail.com>
2019-02-14 11:15:10 -0600

    Crear MBR y EBR

--- struct/info_disk.h
+++ struct/info_disk.h
@@ -37,6 +37,62 @@ struct EBR
     char part_name[16];
 };
 
+char * getCurrentDate ()
+{
+    time_t datetime = time(0);
+    struct tm * tlocal = localtime(&datetime);
+    static char date[18];
+    strftime(date, 18, "%d/%m/%y %H:%M:%S", tlocal);
 
+    return date;
+}
+
+int getRandom ()
+{
+    time_t t;
+    srand((unsigned) time(&t));
+    return rand() % 100;
+}
+
+Partition newPartition (int start)
+{
+    Partition part;
+    part.part_status = 0;
+    part.part_type = '\0';
+    part.part_fit = '\0';
+    part.part_start = start;
+    part.part_size = 0;
+    memset(part.part_name, 0, 16);
+    strcpy(part.part_name, "free");
+
+    return part;
+}
+
+MBR newMBR (int size)
+{
+    MBR mbr;
+    mbr.size = size;
+    memset(mbr.mbr_fecha_creacion, 0, 18);
+    strcpy(mbr.mbr_fecha_creacion, getCurrentDate());
+    mbr.mbr_disk_signature = getRandom();
+
+    for(int i = 0; i < 4; i++)
+        mbr.partitions[i] = newPartition(size);
+    
+    return mbr;
+}
+
+EBR newEBR(int start, int size)
+{
+    EBR ebr;
+    ebr.part_status = 0;
+    ebr.part_fit = '\0';
+    ebr.part_start = start;
+    ebr.part_size = size;
+    ebr.part_next = -1;
+    memset(ebr.part_name, 0, 16);
+
+    return ebr;
+}
 
 #endif // STRUCT_DISK_H_INCLUDED
